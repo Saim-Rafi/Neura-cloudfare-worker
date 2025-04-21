e1b5a609550b3152b926df3ca43c4c88e6a9849e
Saim Rafi <143498300+Saim-Rafi@users.noreply.github.com>
2025-04-22 01:03:11 +0530

    made ChatToDocument Button

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,70 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import OpenAI from 'openai';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		const url = new URL(request.url);
-		switch (url.pathname) {
-			case '/message':
-				return new Response('Hello, World!');
-			case '/random':
-				return new Response(crypto.randomUUID());
-			default:
-				return new Response('Not Found', { status: 404 });
-		}
-	},
-} satisfies ExportedHandler<Env>;
+type Bindings = {
+	OPEN_AI_KEY: string;
+	AI: Ai;
+};
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use(
+	'/*',
+	cors({
+		origin: '*', //Allow requests from your NextJs App
+		allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'],
+		allowMethods: ['GET', 'POST', 'PUT', 'OPTIONS'],
+		exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+		maxAge: 600,
+		credentials: true,
+	})
+);
+
+app.post('/translateDocument', async (c) => {
+	const { documentData, targetLang } = await c.req.json();
+
+	//Generate a summary of the document
+	const summaryResponse = await c.env.AI.run('@cf/facebook/bart-large-cnn', {
+		input_text: documentData,
+		max_length: 1000,
+	});
+
+	//translate the summary into the target language
+	const response = await c.env.AI.run('@cf/meta/m2m100-1.2b', {
+		text: summaryResponse.summary,
+		source_lang: 'english',
+		target_lang: targetLang,
+	});
+
+	return new Response(JSON.stringify(response));
+});
+
+app.post('/chatToDocument', async (c) => {
+	const openai = new OpenAI({
+		apiKey: c.env.OPEN_AI_KEY,
+	});
+
+	const { documentData, question } = await c.req.json();
+
+	const chatCompletion = await openai.chat.completions.create({
+		messages: [
+			{
+				role: 'system',
+				content:
+					'You are a assistant helping the user to chat to the document, I am providing a JSON file of the markdown for the document. Using this , answer the usrs question in the clearest way possible, the document is about ' +
+					documentData,
+			},
+			{
+				role: 'user',
+				content: 'My question is ' + question,
+			},
+		],
+		model: 'gpt-4o',
+		temperature: 0.5,
+	});
+
+	const response =chatCompletion.choices[0].message.content;	
+	return c.json({message : response});
+});
+
+export default app;
