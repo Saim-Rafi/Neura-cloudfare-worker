0b7cd1474f144607d932fedc7370e131891ea44a
Saim Rafi <143498300+Saim-Rafi@users.noreply.github.com>
2025-05-03 19:47:26 +0530

    added ChatToDocument

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 --hono
 --openai
+npx wrangler dev --remote
+
+getting error because i need to purchase the OpenAi plan
--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ app.use(
 	cors({
 		origin: '*', //Allow requests from your NextJs App
 		allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'],
+		//allow content type to the allowed header to fix CORS
 		allowMethods: ['GET', 'POST', 'PUT', 'OPTIONS'],
 		exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
 		maxAge: 600,
@@ -51,8 +52,9 @@ app.post('/chatToDocument', async (c) => {
 			{
 				role: 'system',
 				content:
-					'You are a assistant helping the user to chat to the document, I am providing a JSON file of the markdown for the document. Using this , answer the usrs question in the clearest way possible, the document is about ' +
-					documentData,
+					'You are an assistant helping the user chat with a document. Here is the document data as JSON:\n\n' +
+					JSON.stringify(documentData) +
+					"\n\nUse this data to answer the user's question clearly.",
 			},
 			{
 				role: 'user',
@@ -63,8 +65,11 @@ app.post('/chatToDocument', async (c) => {
 		temperature: 0.5,
 	});
 
-	const response =chatCompletion.choices[0].message.content;	
-	return c.json({message : response});
+	const response = chatCompletion.choices[0].message.content;
+	return c.json({ message: response });
 });
 
 export default app;
+
+// 'You are a assistant helping the user to chat to the document, I am providing a JSON file of the markdown for the document. Using this , answer the usrs question in the clearest way possible, the document is about ' +
+// 					documentData
